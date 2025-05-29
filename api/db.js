7bb8f6e05803a7cfc7645159991679c0dcd0bd18
Matheus Marques <math.marques.silva@gmail.com>
2025-05-28 21:59:54 -0300

    tentando mais uma vez

--- api/db.js
+++ api/db.js
@@ -3,6 +3,6 @@ import mysql from "mysql"
 export const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "willdev123",
+    password: "adsdev123",
     database: "crud"
 })
